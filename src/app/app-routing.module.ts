a0a458a133687cdbcbd9eaf56cd2d6c5ac444122
Phil Lachmann <phil.lachmann@gmail.com>
2018-03-04 12:16:04 -0500

    add card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
+import {SkillsComponent} from './skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent }
+  { path: 'skills', component: SkillsComponent },
+  { path: 'about', component: WelcomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
+import { SkillsComponent } from './skills/skills.component';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import { AuthService } from './auth/auth.service';
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    StopTrainingComponent
+    StopTrainingComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
